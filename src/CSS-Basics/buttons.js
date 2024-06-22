103932d28b6dccf3881327eb474fecad01bc48e6
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-22 15:46:03 +0530

    buttons practiced

---
+++ src/CSS-Basics/buttons.js
@@ -0,0 +1,25 @@
+import "./buttons.css";
+
+export function Subscribe() {
+  return (
+    <div>
+      <button className="subscribe">SUBSCRIBE</button>
+    </div>
+  );
+}
+
+export function Join() {
+  return (
+    <div>
+      <button className="join">JOIN</button>
+    </div>
+  );
+}
+
+export function Tweet() {
+  return (
+    <div>
+      <button className="tweet">Tweet</button>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import reactdom from "react-dom/client";
 import App from "./App";
+import { Subscribe, Join, Tweet } from "./CSS-Basics/buttons";
 
 const root = reactdom.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Subscribe />
+    <Join />
+    <Tweet />
   </React.StrictMode>
 );
