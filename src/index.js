61f3756825b1ee2948f11fb6f067578fa45e0161
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-22 16:20:25 +0530

    button hovers and transistions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import reactdom from "react-dom/client";
 import App from "./App";
-import { Subscribe, Join, Tweet } from "./CSS-Basics/buttons";
+import { Subscribe, Join, Tweet } from "./CSS-Basics/Buttons/buttons";
 
 const root = reactdom.createRoot(document.getElementById("root"));
 
